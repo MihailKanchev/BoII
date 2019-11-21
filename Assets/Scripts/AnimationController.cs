de37116a3f6f065446dd01f25bc7635f165261d7
Mihail Kanchev <mihail@kruso.dk>
2019-11-21 14:57:10 +0100

    Cut down the code by 2. Separated animation from movement. Fixed Bugs.

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class AnimationController : MonoBehaviour
+    {
+        public Animator head, body; //Animator, used for its variables
+        public GameObject spriteBody, spriteHead; //Sprites used to flip the animation
+
+        // Update is called once per frame
+        private void Update()
+        {
+            Animate();
+        }
+
+        //Changes animator variables of head and body accordingly
+        void Animate()
+        {
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                SetAnimatorVriables("isFace", false);
+                SetAnimatorVriables("isVertical", true);
+                body.SetFloat("Speed", 2);
+            }
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                SetAnimatorVriables("isFace", true);
+                SetAnimatorVriables("isVertical", true);
+                body.SetFloat("Speed", 2);
+            }
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                SetAnimatorVriables("isVertical", false);
+                SetAnimatorVriables("flipX", true);
+                body.SetFloat("Speed", 2);
+            }
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                SetAnimatorVriables("isVertical", false);
+                SetAnimatorVriables("flipX", false);
+                body.SetFloat("Speed", 2);
+            }
+            if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+            {
+                body.SetFloat("Speed", 0);
+            }
+        }
+        //Grouped up methods to change animator variables, first is the action, then the boolean.
+        //ex. SetAnimatorVariables("someAction", true)
+        //See actions in the switch cases
+        void SetAnimatorVriables(string variable, bool setter)
+        {
+            switch (variable)
+            {
+                case "isVertical":
+                    if (head.GetBool("isVertical") != setter && body.GetBool("isVertical") != setter)
+                    {
+                        head.SetBool("isVertical", setter);
+                        body.SetBool("isVertical", setter);
+                    }
+                    break;
+                case "isFace":
+                    if (head.GetBool("isFace") != setter)
+                    {
+                        head.SetBool("isFace", setter);
+                    }
+                    break;
+                case "flipX":
+                    if (spriteHead.GetComponent<SpriteRenderer>().flipX != setter && spriteBody.GetComponent<SpriteRenderer>().flipX != setter)
+                    {
+                        spriteBody.GetComponent<SpriteRenderer>().flipX = setter;
+                        spriteHead.GetComponent<SpriteRenderer>().flipX = setter;
+                    }
+                    break;
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,129 +1,25 @@
 ﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Assets.Scripts
 {
-    //Inspector Variables
-    public float playerSpeed; //speed player moves
-    public Animator head;
-    public Animator body;
-    public GameObject spriteBody;
-
-    // Update is called once per frame
-    void Update()
+    public class PlayerController : MonoBehaviour
     {
-        Move(); // Player Movement 
-    }
+        public float playerSpeed; //speed player moves
 
-    void Move()
-    {
-
-        if (Input.GetKey("right") || Input.GetKey("left") || Input.GetKey("up") || Input.GetKey("down"))
+        // Update is called once per frame
+        void Update()
         {
-            
-            if (Input.GetKey("up"))
-            {
-                SetAnimatorVriables("isFace", false);
-                SetAnimatorVriables("isVertical", true);
-                SetAnimatorVriables("isRight", false);
-                spriteBody.GetComponent<SpriteRenderer>().flipX = false;
-                if (transform.position.y > 2.64)
-                {
-                    body.SetFloat("Speed", 0);
-                    transform.Translate(0, 0, 0);
-                }
-                else
-                {
-                    body.SetFloat("Speed", 2);
-                    transform.Translate(0, playerSpeed * Time.deltaTime, 0);
-                }
-            }
-            if (Input.GetKey("down"))
-            {
-                SetAnimatorVriables("isFace", true);
-                SetAnimatorVriables("isVertical", true);
-                SetAnimatorVriables("isRight", false);
-                spriteBody.GetComponent<SpriteRenderer>().flipX = false;
-                if (transform.position.y < -2.24)
-                {
-                    body.SetFloat("Speed", 0);
-                    transform.Translate(0, 0, 0);
-                }
-                else
-                {
-                    body.SetFloat("Speed", 2);
-                    transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
-                }
-            }
-            if (Input.GetKey("left"))
-            {
-                SetAnimatorVriables("isFace", false);
-                SetAnimatorVriables("isVertical", false);
-                SetAnimatorVriables("isRight", false);
-                spriteBody.GetComponent<SpriteRenderer>().flipX = true;
-                if (transform.position.x < -4.3)
-                {
-                    body.SetFloat("Speed", 0);
-                    transform.Translate(0, 0, 0);
-                }
-                else
-                {
-                    body.SetFloat("Speed", 2);
-                    transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
-                }
-            }
-            if (Input.GetKey("right"))
-            {
-                SetAnimatorVriables("isFace", false);
-                SetAnimatorVriables("isVertical", false);
-                SetAnimatorVriables("isRight", true);
-                spriteBody.GetComponent<SpriteRenderer>().flipX = false;
-                if (transform.position.x > 4.3)
-                {
-                    body.SetFloat("Speed", 0);
-                    transform.Translate(0, 0, 0);
-                }
-                else
-                {
-                    body.SetFloat("Speed", 2);
-                    transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
-                }
-            }
+            Move();
+            //Debug.Log("I'm attached to " + gameObject.name);
         }
-        else
+
+        //Sets playerSpeed appropriately
+        void Move()
         {
-            body.SetFloat("Speed", 0);
-        }
-    }
-
-    void SetAnimatorVriables(string variable, bool setter)
-    {
-        switch (variable)
-        {
-            case "isVertical": if (head.GetBool("isVertical")==setter && body.GetBool("isVertical")==setter){
-                                   break;
-                               }
-                               else{
-                                   head.SetBool("isVertical", setter);
-                                   body.SetBool("isVertical", setter);
-                               }
-                               break;
-
-            case "isRight": if (head.GetBool("isRight")==setter && body.GetBool("isRight")==setter){
-                                break;
-                            }
-                            else{
-                                head.SetBool("isRight", setter);
-                                body.SetBool("isRight", setter);
-                            }
-                            break;
-
-            case "isFace": if (head.GetBool("isFace")==setter){
-                               break;
-                           }
-                           else{
-                               head.SetBool("isFace", setter);
-                           }
-                           break;
+            Vector3 v3 = new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"), 0.0F);
+            //Translate takes in a vector
+            transform.Translate(playerSpeed * v3.normalized * Time.deltaTime);
         }
     }
 }
+
