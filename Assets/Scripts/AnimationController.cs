9b85d9cf7071d6774b6c5ae95f5a1a3b1325218f
Mihail Kanchev <mihail@kruso.dk>
2019-11-22 15:13:58 +0100

    Added wall collision.Changed the movement to AddForce.Set inertia.Restructured Isaac animation.

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -2,76 +2,91 @@
 
 namespace Assets.Scripts
 {
+    /*This class acts as Player Character Animation Controller - it sets Isaac's head and body animation variables.
+      It does two actions on Update:
+      1. Sets the speed variable on head and on body. (Head should stop the animation earlier than the body as in the OG - Original Game)
+      !!!The "Speed" variable is set after a check of the player.velocity.magnitude is made!!!
+      2. Takes in player input and sets animation variables accordingly, as well as flips sprites on the X axis if needed.*/
+
+
     class AnimationController : MonoBehaviour
     {
         public Animator head, body; //Animator, used for its variables
         public GameObject spriteBody, spriteHead; //Sprites used to flip the animation
+        private Rigidbody2D player;
 
+        private void Start()
+        {
+            this.player = gameObject.GetComponent<Rigidbody2D>();
+        }
         // Update is called once per frame
         private void Update()
         {
+            SetAnimatorSpeedVariable();
             Animate();
         }
-
         //Changes animator variables of head and body accordingly
         void Animate()
-        {
-            if (Input.GetAxisRaw("Vertical") > 0)
+        {   
+            //Head animation handler
+            if(Input.GetAxisRaw("Horizontal") > 0)
             {
-                SetAnimatorVriables("isFace", false);
-                SetAnimatorVriables("isVertical", true);
-                body.SetFloat("Speed", 2);
+                //Head variables
+                head.SetBool("SideHead", true);
+                head.SetBool("FrontHead", false);
+                head.SetBool("BackHead", false);
+                spriteHead.GetComponent<SpriteRenderer>().flipX = false;
+                //Body variables
+                body.SetBool("isVertical", false);
+                spriteBody.GetComponent<SpriteRenderer>().flipX = false;
+                
+            }
+            if(Input.GetAxisRaw("Horizontal") < 0)
+            {
+                //Head variables
+                head.SetBool("SideHead", true);
+                head.SetBool("FrontHead", false);
+                head.SetBool("BackHead", false);
+                spriteHead.GetComponent<SpriteRenderer>().flipX = true;
+                //Body variables
+                body.SetBool("isVertical", false);
+                spriteBody.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            if(Input.GetAxisRaw("Vertical") > 0)
+            {
+                //Head variables
+                head.SetBool("SideHead", false);
+                head.SetBool("FrontHead", false);
+                head.SetBool("BackHead", true);
+                //Body variables
+                body.SetBool("isVertical", true);
             }
             if (Input.GetAxisRaw("Vertical") < 0)
             {
-                SetAnimatorVriables("isFace", true);
-                SetAnimatorVriables("isVertical", true);
-                body.SetFloat("Speed", 2);
-            }
-            if (Input.GetAxisRaw("Horizontal") < 0)
-            {
-                SetAnimatorVriables("isVertical", false);
-                SetAnimatorVriables("flipX", true);
-                body.SetFloat("Speed", 2);
-            }
-            if (Input.GetAxisRaw("Horizontal") > 0)
-            {
-                SetAnimatorVriables("isVertical", false);
-                SetAnimatorVriables("flipX", false);
-                body.SetFloat("Speed", 2);
-            }
-            if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
-            {
-                body.SetFloat("Speed", 0);
+                //Head variables
+                head.SetBool("SideHead", false);
+                head.SetBool("FrontHead", true);
+                head.SetBool("BackHead", false);
+                //Body variables
+                body.SetBool("isVertical", true);
             }
         }
-        //Grouped up methods to change animator variables, first is the action, then the boolean.
-        //ex. SetAnimatorVariables("someAction", true)
-        //See actions in the switch cases
-        void SetAnimatorVriables(string variable, bool setter)
+        //Sets the Speed variable on body Animator.
+        void SetAnimatorSpeedVariable()
         {
-            switch (variable)
+            if(player.velocity.magnitude < 1.9)
             {
-                case "isVertical":
-                    if (head.GetBool("isVertical") != setter && body.GetBool("isVertical") != setter)
-                    {
-                        head.SetBool("isVertical", setter);
-                        body.SetBool("isVertical", setter);
-                    }
-                    break;
-                case "isFace":
-                    if (head.GetBool("isFace") != setter)
-                    {
-                        head.SetBool("isFace", setter);
-                    }
-                    break;
-                case "flipX":
-                    if (spriteHead.GetComponent<SpriteRenderer>().flipX != setter && spriteBody.GetComponent<SpriteRenderer>().flipX != setter)
-                    {
-                        spriteBody.GetComponent<SpriteRenderer>().flipX = setter;
-                        spriteHead.GetComponent<SpriteRenderer>().flipX = setter;
-                    }
-                    break;
+                head.SetBool("SideHead", false);
+                head.SetBool("FrontHead", true);
+                head.SetBool("BackHead", false);
+            }
+            if (player.velocity.magnitude > 0.2)
+            {
+                body.SetFloat("Speed", 2);
+            }
+            else
+            {
+                body.SetFloat("Speed", 0);
             }
         }
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,28 +2,37 @@
 
 namespace Assets.Scripts
 {
+
+    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerController : MonoBehaviour
     {
         public float playerSpeed; //speed player moves
-        private Vector3 direction;
+        private Vector2 direction; //direction of movement
+        private Rigidbody2D player;
 
+        private void Start()
+        {
+            player = gameObject.GetComponent<Rigidbody2D>();
+        }
         // Update is used for inputs.
         void Update()
         {
-            direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"),0);
+            ReceiveInput();
         }
-
         //Fixed update is mainly used for physics checks.
         private void FixedUpdate()
         {
             Move();
         }
-
-        //Sets playerSpeed appropriately
+        //Normalizes the "direction" and adds force to the rigidbody.
         void Move()
         {
-            Vector3 newPosition = transform.position + (direction.normalized * playerSpeed * Time.deltaTime);
-            gameObject.GetComponent<Rigidbody>().MovePosition(newPosition);
+            player.AddForce(direction.normalized * playerSpeed * Time.deltaTime, ForceMode2D.Force);
+        }
+        //Listens for Horizontal or Vertical input and saves it into a Vectoe2.
+        void ReceiveInput()
+        {
+            direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         }
     }
 }
